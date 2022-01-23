fd408dd679c641a09f87842059f7d557d7053d58
Sinan TEKİN <37111233+sinan73k1n@users.noreply.github.com>
2022-01-24 01:05:06 +0200

    Yeni baslangic

---
+++ Assets/Scripts/Ses/SesKutusu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SesKutusu : MonoBehaviour
+{
+    public static SesKutusu instance;
+    AudioSource audioSource;
+    private void Awake()
+    {
+        instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void Play(NameOfAudioClip name) { audioSource.PlayOneShot(GetASoundClip.Hangi(name)); }
+    public void PlayIfDontPlay(NameOfAudioClip name) { if(!audioSource.isPlaying) audioSource.PlayOneShot(GetASoundClip.Hangi(name)); }
+    public void Stop() { audioSource.Stop(); }
+}
+
