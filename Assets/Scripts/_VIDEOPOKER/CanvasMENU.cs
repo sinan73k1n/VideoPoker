489394037c90f25e6d43a9aa0a04e8d97884f19e
Sinan TEKİN <37111233+sinan73k1n@users.noreply.github.com>
2022-01-26 05:27:39 +0200

    UI GOREV UI MENU, Poker hesaplama düzeltildi

---
+++ Assets/Scripts/_VIDEOPOKER/CanvasMENU.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CanvasMENU : MonoBehaviour
+{
+    [SerializeField] Button _btnHow, _btnIstatistic, _btnAyar, _btnBack, _btnExit;
+    [SerializeField] GameObject _goHow, _goIstatistic, _goAyar;
+
+    void Start()
+    {
+
+        AtaHandle();
+
+    }
+    void AtaHandle()
+    {
+        _btnHow.onClick.AddListener(() => HandleOpen(_goHow));
+        _btnIstatistic.onClick.AddListener(() => HandleOpen(_goIstatistic));
+        _btnAyar.onClick.AddListener(() => HandleOpen(_goAyar));
+        _btnBack.onClick.AddListener(HandleBack);
+        _btnExit.onClick.AddListener(()=> StartCoroutine(HandleExit()));
+    }
+
+    void HandleOpen(GameObject hangi)
+    {
+        SesKutusu.instance.Play(NameOfAudioClip.VideoPokerTusaBas);
+       // hangi.SetActive(true);
+    }
+
+    void HandleBack()
+    {
+        SesKutusu.instance.Play(NameOfAudioClip.VideoPokerTusaBas);
+        gameObject.SetActive(false);
+    }
+    IEnumerator HandleExit()
+    {
+        SesKutusu.instance.Play(NameOfAudioClip.VideoPokerTusaBas);
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+}
